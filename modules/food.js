5f951dd60c08ff2ba111ef309e7cd4ee73abbbaf
Victor Litzanov <vlitzanov@yahoo.com>
2020-08-30 01:07:04 -0400

    v2 update files

--- modules/food.js
+++ modules/food.js
@@ -1,5 +1,4 @@
 "use strict";
-const o = require('../auths/food');
 const g = require('../gauths');
 const d = require("discord.js");
 const b = new d.Client();
@@ -8,14 +7,21 @@ async function start(){try{await b.login(g.token);return true;}catch(e){return f
 
 start();
 
+const chan = ''
+
 b.on("ready", () => {
 	console.log("ready");
 })
 
 b.on('message', async (m) => {
 
-	if (m.content === g.pref + "foodrate"){
-		if (m.channel.id === o.channel){
+	if (m.channel.id === chan){
+		if (m.content === g.pref + "help"){
+
+			await m.channel.send("Just send a photo or video or mp3 or something.\nI'll do the work for you.");
+
+		} else if (m.attachments.size > 0){
+		
 			await m.react('😁');
 			await m.react('🙂');
 			await m.react('😐');
--- modules/vc.js
+++ modules/vc.js
@@ -1,72 +1,81 @@
 "use strict";
-const o = require('../auths/vc');
 const g = require('../gauths');
 const d = require("discord.js");
 const b = new d.Client();
+const f = require('fs');
+const req = require('request');
 
 async function start(){try{await b.login(g.token);return true;}catch(e){return false}}
 
 start();
 
-b.on("ready", () => {
-	console.log("ready");
+const category = ''
+const callrole = ''
+const callchannel = ''
+
+var interval
+
+var channels
+var vchan
+
+b.on('ready', async () => {
+	
+	channels = await b.channels.fetch(category)
+	
+	vchan = await b.channels.fetch(callchannel)
+	
+	var vcs = await channels.children.filter(channel => channel.type == "voice")
+		var flag = true;
+		vcs.each(vc => {
+			vc.members.each(member => {
+				member.roles.add(channels.guild.roles.cache.get(callrole))
+			})
+		})
+	
+	console.log("ready")
 })
 
 b.on('voiceStateUpdate', async (oldState, newState) => {
 
-	if (oldState.channel === null && newState.channel !== null) {
-		console.log(new Date() + "``Trigger:`` **" + newState.member.user.tag + " joined** " + newState.channel.name);
-		if (newState.channel.parentID == o.category) {
-			await newState.member.roles.add(newState.guild.roles.cache.get(o.callrole))
-    	}
-    	
-	} else if (oldState.channel !== null && newState.channel !== null && oldState.channel.id !== newState.channel.id) {
-		console.log(new Date() + "``Trigger:`` **" + newState.member.user.tag + " moved** " + oldState.channel.name + " -> " + newState.channel.name)
-		if (newState.channel.parentID == o.category) {
-			await newState.member.roles.add(oldState.guild.roles.cache.get(o.callrole))
-		} else {
-			await newState.member.roles.remove(oldState.guild.roles.cache.get(o.callrole))
-		}
-	} else if (oldState.channel !== null && newState.channel === null) {
-		console.log(new Date() + "``Trigger:`` **" + oldState.member.user.tag + " left**" + oldState.channel.name);
-		if (oldState.channel.parentID == o.category){
-			await oldState.member.roles.remove(oldState.guild.roles.cache.get(o.callrole))
-			if (oldState.guild.roles.cache.get(o.callrole).members.size == 0) {
-				var fetched = await oldState.guild.channels.cache.get(o.callchannel).messages.fetch({limit: 99});
-				await oldState.guild.channels.cache.get(o.callchannel).bulkDelete(fetched)
-				var message = await oldState.guild.channels.cache.get(o.callchannel).send("Use this channel for all VC content amongst yourselves.\n***It is only temporary and will be deleted once everyone leaves.***\nYou can save stuff to your DM by adding the '💾' reaction.")
-				await message.pin()
-			}
-		}
+	if ((oldState.channelID === null || oldState.channelID === undefined) && newState.channelID !== null)
+	{
+		
+		clearInterval(interval)
+		await newState.member.roles.add(newState.guild.roles.cache.get(callrole))
+		
 	}
+	if ((oldState.channelID !== null || oldState.channelID !== undefined) && newState.channelID === null)
+	{
+		
+		await oldState.member.roles.remove(oldState.guild.roles.cache.get(callrole))	
+		var vcs = await channels.children.filter(channel => channel.type == "voice")
+		var flag = true;
+		vcs.each(vc => {
+			if (vc.members.size > 0) {
+				flag = false
+			}
+		})
+		
+		if (flag) {
+			interval = setInterval(tick, 1000);
+		}
+			
+	}
+
 })
 
-async function download(url,filename){
-    await req.get(url)
-        .on('error', console.error)
-        .pipe(f.createWriteStream(filename));
+function tick() {
+	
+	vchan.messages.fetch({limit: 1}).then(fetched => {
+	
+		vchan.bulkDelete(fetched.filter(m => m.id !== '731768253038198824'))
+		
+		if (fetched.size == 0){
+	
+			clearInterval(interval)
+	
+		}
+		
+	})
+	
 }
-
-b.on('message', async (m) => {
-  	
-	if (m.channel.id == o.callchannel){
-		if (m.author.id !== b.user.id) {
-			await m.react('💾');
-		}
-	}
-
-})
-
-b.on("messageReactionAdd", async (messageReaction, user) => {
-
-	if (user.id != b.user.id){
-		if (messageReaction.message.channel.id == o.callchannel) {
-			if (messageReaction.emoji.toString() === '💾') {
-				var temp = [];
-				var r = await user.createDM();
-				r.send(messageReaction.message);
-			}
-		}
-	}
-
-})
